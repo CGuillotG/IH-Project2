7938ea6ae94b2f4c62c1bb628107a39d0b494961
Carlos Ortiz <ca.ortiz.pacheco@gmail.com>
2019-02-16 17:34:35 -0600

    problemas con auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,11 @@ router.post("/signup",(req,res,next)=>{
     if(req.body.password != req.body.password2){
         return res.render("auth/signup",{ error : "Please type the same password"})
     }
+    console.log(User)
     User.register({...req.body} , req.body.password)
     .then(()=>{
         passport.authenticate("local")(req,res,()=>{
+            return res.redirect("/profile")
         })
     })
     .catch(e=>{
@@ -28,7 +30,22 @@ router.post("/signup",(req,res,next)=>{
     })
 })
 
+router.get("/login",(req,res,next)=>{
+    res.render("/auth/login")
 
+})
 
+router.post("/login",passport.authenticate("local"),(req,res,next)=>{
+    res.redirect("/profile")
+})
+
+router.get("/profile",isLogged,(req,res,next)=>{
+    res.render("auth/profile")
+})
+
+router.get("/logout",(req,res,next)=>{
+    req.logOut()
+    res.redirect("/")
+})
 
 module.exports = router
