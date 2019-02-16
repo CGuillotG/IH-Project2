63f75bbeaa8af63ff97df6e518ddc3d85099fcc2
Carlos Ortiz <ca.ortiz.pacheco@gmail.com>
2019-02-15 22:58:20 -0600

    15022019_Basic_2

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,29 @@ let passport = require("passport")
 
 let User = require("../models/User")
 
+function isLogged(req,res,next){
+    if(req.isAuthenticated()) return next()
+    return res.redirect("/login")
+}
 
-router.get("/login",(req,res,next)=>{
-    res.render("/auth/login")
+
+
+router.get("/signup",(req,res,next)=>{
+    res.render("auth/signup")
+})
+
+router.post("/signup",(req,res,next)=>{
+    if(req.body.password != req.body.password2){
+        return res.render("auth/signup",{ error : "Please type the same password"})
+    }
+    User.register({...req.body} , req.body.password)
+    .then(()=>{
+        passport.authenticate("local")(req,res,()=>{
+        })
+    })
+    .catch(e=>{
+        res.render("auth/signup", {e})
+    })
 })
 
 
