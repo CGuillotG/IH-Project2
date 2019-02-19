e9990c0ec13eceb6d86d8fdf80ab630fae325bd7
CGuillotG <c.guillot@outlook.com>
2019-02-19 10:48:40 -0600

    Reajuste completo de login y signup

--- routes/auth.js
+++ routes/auth.js
@@ -4,49 +4,57 @@ let passport = require("passport")
 let User = require("../models/User")
 
 function isLogged(req,res,next){
-    if(req.isAuthenticated()) return next()
-    return res.redirect("/login")
+  if(req.isAuthenticated()) return next()
+  return res.redirect("/login")
 }
-
-
-
-router.get("/signup",(req,res,next) => {
-    res.render("auth/signup")
+//we should move this
+router.get("/profile",isLogged,(req,res,next) => {
+  res.render("auth/profile")
 })
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { error: req.flash("error") });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
 
 router.post("/signup", (req,res,next) => {
-    if(req.body.password != req.body.password2){
-        return res.render("auth/signup", {e})
+  if (req.body.username === "" || req.body.password === "") {
+      res.render("auth/signup", { error: "Indicate username and password" });
+      return;
+  }
+  if(req.body.password != req.body.password2){
+      return res.render("auth/signup", {error: "Password doesn't match"})
+  }
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+        return res.render("auth/signup", { error: "The username already exists" });
     }
-    User.register({...req.body}, req.body.password)
-    .then(()=>{
-        passport.authenticate("local")(req,res, () => {
-            return res.redirect("/profile")
-        })
-    })
-    .catch(e=>{
-        res.render("auth/signup", {e})
-    })
+  })
+ 
+  User.register({...req.body}, req.body.password)
+  .then(()=>{
+      passport.authenticate("local")(req,res, () => {
+      res.redirect("/profile")
+      })
+  })
+  .catch(e=>{
+      res.render("auth/signup", {error: e})
+  })
 })
 
-router.get("/login",(req,res,next) => {
-    res.render("auth/login")
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
-})
-
-router.post("/login", passport.authenticate("local"), (req,res,next) => {
-    res.redirect("/profile")
-})
-
-router.get("/profile",isLogged,(req,res,next) => {
-
-        res.render("auth/profile")
-    
-})
-
-router.get("/logout",(req,res,next) => {
-    req.logOut()
-    res.redirect("/")
-})
-
-module.exports = router
+module.exports = router;
