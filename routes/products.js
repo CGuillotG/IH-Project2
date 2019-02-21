d4f8b4b3c7d38cda35f548478f16b17ff502a6f7
CGuillotG <c.guillot@outlook.com>
2019-02-21 13:18:12 -0600

    Added db aggregate to orders

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router  = express.Router();
 let {isLogged} = require('../helpers/middlewares')
 let {isSeller} = require('../helpers/middlewares')
+let mongoose = require("mongoose")
 
 let Product = require ("../models/Product")
 
@@ -10,8 +11,27 @@ router.get("/buyer", isLogged, (req,res,next) => {
   res.render("buyer/buyer")
 })
 
-router.get("/buyer/orders",(req,res,next) => {
-  res.send("My Orders")
+router.get("/buyer/orders", isLogged, (req,res,next) => {  
+  console.log(req.user._id)
+  // Product.find({ 'order.buyers.buyer' : {'$eq': req.user._id}})
+  Product.aggregate([
+    {
+      '$lookup': {
+        'from': 'orders', 
+        'localField': 'order', 
+        'foreignField': '_id', 
+        'as': 'ordenes'
+      }
+    }, {
+      '$match': {
+        'ordenes.buyers.buyer': mongoose.Types.ObjectId(req.user._id)
+      }
+    }
+  ])
+  .then(prod=>{
+    // res.json(prod)
+    res.render("orders/orders", prod)
+  })
 })
 
 router.get("/buyer/buyerproducts",(req,res,next) => {
